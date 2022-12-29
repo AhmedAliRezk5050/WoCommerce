54b49bf159902700e4778ec0af2504d7f12c6ae7
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2022-12-29 19:18:37 +0300

    Add Custom Error Response class

---
+++ API/Errors/AppErrorResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace API.Errors;
+
+public class AppErrorResponse
+{
+    public int StatusCode { get; }
+    public string? Message { get; }
+
+    public AppErrorResponse(int statusCode, string? message = null)
+    {
+        StatusCode = statusCode;
+        Message = message ?? GetDefaultErrorMessage(StatusCode);
+    }
+
+    private static string? GetDefaultErrorMessage(int statusCode)
+    {
+        return statusCode switch
+        {
+            400 => "Bad Request",
+            401 => "Unauthorized",
+            404 => "Not Found",
+            500 => "Internal server Error",
+            _ => null
+        };
+    }
+}
