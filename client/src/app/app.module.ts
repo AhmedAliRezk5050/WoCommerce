236596ba5e2eb2890d78073ba52aec716eb87262
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2023-01-05 12:01:12 +0300

    Client Add Font Awesome

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import {faReact as fabReact} from '@fortawesome/free-brands-svg-icons'
 
 @NgModule({
   declarations: [
@@ -12,9 +18,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(fasStar, farStar, fabReact);
+  }
+}
