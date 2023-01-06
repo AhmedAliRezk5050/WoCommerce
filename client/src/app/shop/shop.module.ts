33c29615cec1afb16ea9b921e152388b3a28fd94
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2023-01-06 20:48:54 +0300

    Client Add product-item component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,20 +2,24 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ShopComponent} from './shop.component';
 import {ShopRoutingModule} from "./shop-routing.module";
+import {ProductItemComponent} from './product-item/product-item.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 
 @NgModule({
-    declarations: [
-        ShopComponent
-    ],
-    exports: [
-        ShopComponent
-    ],
-    imports: [
-        CommonModule,
-        ShopRoutingModule,
+  declarations: [
+    ShopComponent,
+    ProductItemComponent
+  ],
+  exports: [
+    ShopComponent
+  ],
+  imports: [
+    CommonModule,
+    ShopRoutingModule,
+    FontAwesomeModule,
 
-    ]
+  ]
 })
 export class ShopModule {
 }
